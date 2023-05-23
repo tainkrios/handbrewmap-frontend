2e22cd29ca22f5a33d2feadba5746d0ee751a058
tainkrios <tain.krios@gmail.com>
2023-05-23 18:34:38 +0200

    fix url

--- src/shared/getPlaceOpenStatus.js
+++ src/shared/getPlaceOpenStatus.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const getPlaceOpenStatus = async (placeId) => {
-  let url = `https://handbrew-server.ivanzhdanov.ru/${placeId}`
+  let url = `https://88.210.9.231:1963/${placeId}`
   const config = {
     headers: {
       'Content-Type': 'application/json',
