51b4906fe912814c4ab9a175e27e6cc5dc893ccf
tainkrios <tain.krios@gmail.com>
2023-07-05 17:39:00 +0200

    🚀

--- src/components/burger/ui/Burger.js
+++ src/components/burger/ui/Burger.js
@@ -23,22 +23,23 @@ export const Burger = () => {
             If you have any questions or suggestions, write ➡️{' '}
             <a href='https://wa.me/+79817618313'>@tainkrios</a>
           </p>
-          <p>Donation</p>
           <p>
-            <a href='https://www.paypal.com/paypalme/tainkrios'>PayPal</a>
-          </p>
-          <p>
-            <a href='https://revolut.me/tainkrios'>Revolut</a>
-          </p>
-          <p>
-            All money will go to buy a cup of coffee ☕️ and explore about new
-            places in Berlin{' '}
+            Your donation will help fund my adventures in Berlin as I explore
+            new places and discover hidden gems. With your support, I'll be able
+            to enjoy a cup of coffee ☕️ as I take in the sights and sounds of
+            the city. Thank you for helping me make the most of my travels!
             <span>
               <img
                 src='https://static.xx.fbcdn.net/rsrc.php/v3/yO/r/uozz7Eg-cMn.png'
                 alt='brandenburger-tor-icon'
               />
             </span>
+          </p>
+          <p>
+            <a href='https://www.paypal.com/paypalme/tainkrios'>PayPal</a>
+          </p>
+          <p>
+            <a href='https://revolut.me/tainkrios'>Revolut</a>
           </p>
         </div>
       </div>
